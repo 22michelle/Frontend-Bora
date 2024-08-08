a9be7123d2c7e2e6db0a91627ad17c3265442df2
Michelle <michellemejiaparra4@gmail.com>
2024-08-08 12:23:29 -0500

    Bora

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import store from "./redux/store.js";
 import App from "./App.jsx";
 import "./App.css";
+import axios from "axios";
 import { BrowserRouter as Router } from "react-router-dom";
 
 axios.defaults.baseURL = "https://backend-bora.onrender.com";
